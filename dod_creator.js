9a6090a22f200eb86c21d70686b4ffab9c290e96
Jonas Carlsson <jonas.ba.carlsson@gmail.com>
2015-12-24 01:01:16 +0100

    yay did the tutorial

--- dod_creator.js
+++ dod_creator.js
@@ -1,11 +1,36 @@
 Tasks = new Mongo.Collection("tasks");
- 
+
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function (){ 
+      return Tasks.find({
+        $or: [
+             {private: {$ne: true}},
+             {owner: this.userId}
+          ]
+      });
+  });
+  initMethods();
+}
+
 if (Meteor.isClient) {
   // This code only runs on the client
+  Meteor.subscribe("tasks");
+  initMethods();
   Template.body.helpers({
-   tasks: function () {
-       return Tasks.find({}, {sort: {createdAt: -1}});
-   }
+    tasks: function () {
+             if(Session.get("hideCompleted")){
+               return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}}); 
+             }else{
+               return Tasks.find({}, {sort: {createdAt: -1}});
+             }
+           },
+    hideCompleted: function(){
+                     return Session.get("hideCompleted");
+                   },
+    incompleteCount: function(){
+                       return Tasks.find({checked: {$ne:true}}).count();
+                     }
+                       
   });
    
    Template.body.events({
@@ -16,25 +41,66 @@ if (Meteor.isClient) {
        var text = event.target.text.value;
        
        // Insert a task into the collection
-       Tasks.insert({
-         text: text,
-         createdAt: new Date() // current time
-       });
-       
+       Meteor.call("addTask", text);
        // Clear form
        event.target.text.value = "";
+     },
+     "change .hide-completed input": function (event) {
+       Session.set("hideCompleted", event.target.checked);
      }
+   });
+
+   Template.task.helpers({
+     isOwner: function(){
+                return this.owner == Meteor.userId();
+              }
    });
 
   Template.task.events({
     "click .toggle-checked": function () {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
-       $set: {checked: ! this.checked}
-    });
+      // Set the checked property to the opposite of its current value
+       Meteor.call("setChecked", this._id, ! this.checked);
    },
    "click .delete": function () {
-     Tasks.remove(this._id);
-   }
+     Meteor.call("deleteTask", this._id);
+   },
+    "click .toggle-private":function () {
+      Meteor.call("setPrivate", this._id, ! this.private);
+    }
    });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  function initMethods(){
+    Meteor.methods({
+      addTask: function(text){
+                 // Make sure user is logged in
+                 if (! Meteor.userId()){
+                    throw new Meteor.error("not-authorized");
+                 }
+                 Tasks.insert({
+                   text:text,
+                   createdAt:new Date(),
+                   owner: Meteor.userId(),
+                   username: Meteor.user().username
+                 });
+               },
+      deleteTask: function(taskId){
+                     Tasks.remove(taskId)
+                   },
+      setChecked: function (taskId, setChecked){
+                    Tasks.update(taskId, { $set: { checked: setChecked} });
+                  },
+      setPrivate: function (taskId, setToPrivate){
+                    var task = Tasks.findOne(taskId);
+                     if (task.owner !== Meteor.userId()) {
+                       throw new Meteor.Error("not-authorized");
+                     }
+
+                     Tasks.update(taskId, { $set: { private: setToPrivate } });
+                  }
+    });
+  }
 }
