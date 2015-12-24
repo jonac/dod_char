c9ad8377bdc73cdf88a85fc2a36c402d2b4d6512
Jonas Carlsson <jonas.ba.carlsson@gmail.com>
2015-12-24 10:02:33 +0100

    more structuring

--- server/server.js
+++ server/server.js
@@ -7,32 +7,4 @@ if (Meteor.isServer) {
           ]
       });
   });
-    Meteor.methods({
-      addTask: function(text){
-                 // Make sure user is logged in
-                 if (! Meteor.userId()){
-                    throw new Meteor.error("not-authorized");
-                 }
-                 Tasks.insert({
-                   text:text,
-                   createdAt:new Date(),
-                   owner: Meteor.userId(),
-                   username: Meteor.user().username
-                 });
-               },
-      deleteTask: function(taskId){
-                     Tasks.remove(taskId)
-                   },
-      setChecked: function (taskId, setChecked){
-                    Tasks.update(taskId, { $set: { checked: setChecked} });
-                  },
-      setPrivate: function (taskId, setToPrivate){
-                    var task = Tasks.findOne(taskId);
-                     if (task.owner !== Meteor.userId()) {
-                       throw new Meteor.Error("not-authorized");
-                     }
-
-                     Tasks.update(taskId, { $set: { private: setToPrivate } });
-                  }
-    });
 }
